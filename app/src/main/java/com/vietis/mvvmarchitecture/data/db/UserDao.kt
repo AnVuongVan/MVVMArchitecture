fd71c8c51e4e1b82507dabaad36eead6c1934add
Vuong Van An <vananbk99@gmail.com>
2020-10-16 23:26:10 +0700

    Room database setup

---
+++ app/src/main/java/com/vietis/mvvmarchitecture/data/db/UserDao.kt
@@ -0,0 +1,18 @@
+package com.vietis.mvvmarchitecture.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.vietis.mvvmarchitecture.data.db.entities.CURRENT_USER_ID
+import com.vietis.mvvmarchitecture.data.db.entities.User
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: User) : Long
+
+    @Query("SELECT * FROM user WHERE uid = $CURRENT_USER_ID")
+    fun getUser() : LiveData<User>
+}
